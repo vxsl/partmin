31329589a7476b52a7d08a9aa2c2d88d24411db8
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2024-01-24 12:13:09 -0500

    discord embed displays bullet points if the item has any

--- ts/src/notifications/discord/util.ts
+++ ts/src/notifications/discord/util.ts
@@ -7,33 +7,46 @@ const platformIcons: Record<Platform, string> = {
   fb: "https://www.facebook.com/favicon.ico",
 };
 
-export const convertItemToDiscordEmbed = (item: Item) =>
-  new Discord.EmbedBuilder()
-    .setTitle(item.details.title ?? null)
-    .setDescription(
-      [
-        `${
-          item.details.price
-            ? `**$${parseFloat(`${item.details.price}`).toFixed(2)}**`
-            : undefined
-        }`,
-        item.computed?.locationLinkMD ??
-          item.details.location ??
-          (item.details.lat && item.details.lon
-            ? `(${item.details.lat}, ${item.details.lon})`
-            : undefined),
-      ]
-        .filter(notUndefined)
-        .join(" / ")
-    )
-    .setURL(item.url)
-    .setImage(item.imgURLs[0] ?? null)
-    .setThumbnail(item.imgURLs[1] ?? null)
-    .setFooter({
-      text: item.platform,
-      iconURL: platformIcons[item.platform],
-    })
-    .setTimestamp(new Date());
+export const convertItemToDiscordEmbed = (item: Item) => {
+  const descriptionHeader = [
+    `${
+      item.details.price
+        ? `**$${parseFloat(`${item.details.price}`).toFixed(2)}**`
+        : undefined
+    }`,
+    item.computed?.locationLinkMD ??
+      item.details.location ??
+      (item.details.lat && item.details.lon
+        ? `(${item.details.lat}, ${item.details.lon})`
+        : undefined),
+  ]
+    .filter(notUndefined)
+    .join(" / ");
+
+  return (
+    new Discord.EmbedBuilder()
+      .setTitle(item.details.title ?? null)
+      .setDescription(
+        [
+          descriptionHeader,
+          item.computed?.bulletPoints?.map((p) => `- ${p}`).join("\n") ?? "",
+        ]
+          .filter(Boolean)
+          .join("\n")
+        // `${descriptionHeader}\n\n${
+        //   item.computed?.bulletPoints?.map((p) => `- ${p}`).join("\n") ?? ""
+        // }`
+      )
+      .setURL(item.url)
+      .setImage(item.imgURLs[0] ?? null)
+      // .setThumbnail(item.imgURLs[1] ?? null)
+      .setFooter({
+        text: item.platform,
+        iconURL: platformIcons[item.platform],
+      })
+      .setTimestamp(new Date())
+  );
+};
 
 export const attachVideosToEmbed = (
   embed: Discord.EmbedBuilder,
