c1d24f2b3c70cd5d10e2a0542bb95de2e0d7beae
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2024-01-24 10:45:42 -0500

    cache kijiji RSS url

--- ts/src/kijiji/index.ts
+++ ts/src/kijiji/index.ts
@@ -1,11 +1,19 @@
+import fs from "fs";
 import { WebDriver } from "selenium-webdriver";
 import { Config } from "types/config.js";
+import { tmpDir } from "../constants.js";
 import { getKijijiRSS, scrapeItems } from "./util/index.js";
 
 let rssURL: string | undefined;
 
 export const kijijiPre = async (config: Config, driver: WebDriver) => {
-  rssURL = await getKijijiRSS(config, driver);
+  try {
+    rssURL = await fs.promises.readFile(`${tmpDir}/kijiji-rss-url`, "utf-8");
+  } catch {}
+  if (!rssURL) {
+    rssURL = await getKijijiRSS(config, driver);
+    await fs.promises.writeFile(`${tmpDir}/kijiji-rss-url`, rssURL);
+  }
 };
 
 export const kijijiMain = async (config: Config, driver: WebDriver) => {
