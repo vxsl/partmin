272f9a56b18ae5d27b2af3ad8a2fdbe6d84eabd1
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2024-01-24 11:54:28 -0500

    make blacklist checking more robust, informative blacklist logging

--- ts/src/process.ts
+++ ts/src/process.ts
@@ -8,6 +8,42 @@ import { log, verboseLog } from "./util/misc.js";
 
 dotenv.config();
 
+let blacklist: string[] | undefined;
+
+const findBlacklistedWords = (i: Item): string[] | null => {
+  const result: string[] = [];
+
+  if (
+    blacklist?.some((_b) => {
+      const b = _b.toLowerCase();
+
+      const descriptionMatch = i.details.description?.toLowerCase().includes(b);
+      if (descriptionMatch) {
+        result.push(`'${_b}' in item ${i.id}'s description`);
+        return true;
+      }
+
+      const titleMatch = i.details.title?.toLowerCase().includes(b);
+      if (titleMatch) {
+        result.push(`'${_b}' in item ${i.id}'s title`);
+        return true;
+      }
+
+      const locationMatch = i.details.location?.toLowerCase().includes(b);
+      if (locationMatch) {
+        result.push(`'${_b}' in item ${i.id}'s location`);
+        return true;
+      }
+
+      return false;
+    })
+  ) {
+    return result;
+  }
+
+  return null;
+};
+
 export type Platform = "kijiji" | "fb";
 
 export type Item = {
@@ -32,11 +68,6 @@ export type Item = {
   );
   imgURLs: string[];
 };
-
-let blacklist: string[] | undefined;
-
-export const itemIsBlacklisted = (item: Item) =>
-  blacklist?.some((b) => JSON.stringify(item).toLowerCase().includes(b));
 
 interface SeenItemDict {
   [k: string]: 1 | undefined;
@@ -85,58 +116,61 @@ export const withUnseenItems = async <T>(
 
 export const processItems = async (config: Config, items: Item[]) => {
   if (!blacklist) {
-    blacklist = config.search.blacklist.map((b) => b.toLowerCase());
+    blacklist = config.search.blacklist?.map((b) => b.toLowerCase());
   }
+  const blacklistLog: string[] = [];
+  return await withUnseenItems(
+    items,
+    { markAsSeen: true },
+    async (unseenItems) => {
+      const { targets, blacklisted } = await unseenItems.reduce<
+        Promise<{
+          targets: Item[];
+          blacklisted: Item[];
+        }>
+      >(
+        async (filteredPromises, item) => {
+          const result = await filteredPromises;
 
-  await withUnseenItems(items, async (unseenItems) => {
-    const { targets, blacklisted } = await unseenItems.reduce<
-      Promise<{
-        targets: Item[];
-        blacklisted: Item[];
-      }>
-    >(
-      async (filteredPromises, item) => {
-        const result = await filteredPromises;
-        const isBlacklisted = await itemIsBlacklisted(item);
-        if (isBlacklisted) {
-          result.blacklisted.push(item);
-        } else {
-          result.targets.push(item);
-        }
-        return result;
-      },
-      Promise.resolve({
-        targets: [],
-        blacklisted: [],
-      })
-    );
-
-    const platform = items[0].platform;
-
-    for (const item of targets) {
-      if (item.details.location || !item.details.lat || !item.details.lon)
-        continue;
-      item.details.location = await generateLocationLink(
-        item.details.lat,
-        item.details.lon
+          const blacklistOccurrences = findBlacklistedWords(item);
+          if (blacklistOccurrences) {
+            blacklistLog.push(...blacklistOccurrences);
+            result.blacklisted.push(item);
+          } else {
+            result.targets.push(item);
+          }
+          return result;
+        },
+        Promise.resolve({
+          targets: [],
+          blacklisted: [],
+        })
       );
+
+      const platform = items[0].platform;
+
+      for (const item of targets) {
+        if (item.details.location || !item.details.lat || !item.details.lon)
+          continue;
+        item.details.location = await generateLocationLink(
+          item.details.lat,
+          item.details.lon
+        );
+      }
+
+      log("\n=======================================================");
+      log(`${platform}: ${targets.length} new results${VERBOSE ? ":" : "."}`);
+      verboseLog(targets);
+      if (blacklisted.length) {
+        log(`${blacklisted.length} blacklisted:`);
+        log(blacklistLog.map((b) => `  - found ${b}`).join("\n"));
+      }
+      // TODO compute duplicates
+      log("----------------------------------------\n");
+
+      // process.exit();
+
+      return targets;
     }
-
-    log("\n=======================================================");
-    log(`${platform}: ${targets.length} new results${VERBOSE ? ":" : "."}`);
-    verboseLog(targets);
-    if (blacklisted.length) {
-      log(
-        `${platform}: ${blacklisted.length} new but blacklisted${
-          VERBOSE ? ":" : "."
-        }`
-      );
-    }
-    // TODO compute duplicates
-    log("----------------------------------------\n");
-
-    process.exit();
-
-    return targets;
-  });
+  );
 };
