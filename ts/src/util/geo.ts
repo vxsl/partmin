e686f0a1ba72332871a83187f1e9a0dc3fd1f3a7
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2024-01-24 11:18:51 -0500

    refactor isWithinRadii

--- ts/src/util/geo.ts
+++ ts/src/util/geo.ts
@@ -1,10 +1,19 @@
 import haversine from "haversine";
 import { Config } from "../types/config.js";
 import { verboseLog } from "./misc.js";
+import { tmpDir } from "../constants.js";
+import { readJSON, writeJSON } from "./io.js";
+import axios from "axios";
 
-const decodeMapDevelopersURL = (
-  url: string
-): { lat: number; lon: number; radius: number }[] => {
+// TODO move to types?
+// TODO ensure everything is using this instead of { lat, lon, radius }
+export type Radius = {
+  lat: number;
+  lon: number;
+  diam: number;
+};
+
+export const decodeMapDevelopersURL = (url: string): Radius[] => {
   const circlesParam = url.match(/circles=([^&]*)/)?.[1];
   if (!circlesParam) {
     throw new Error("Error parsing mapDevelopersURL");
@@ -16,24 +25,23 @@ const decodeMapDevelopersURL = (
     return {
       lat: parseFloat(lat),
       lon: parseFloat(lon),
-      radius: parseFloat(radius) / 1000,
+      diam: parseFloat(radius) / 1000,
     };
   });
 };
 
-export const withinRadii = (lat: number, lon: number, config: Config) => {
+export const isWithinRadii = (lat: number, lon: number, config: Config) => {
   const radii = decodeMapDevelopersURL(config.search.location.mapDevelopersURL);
   verboseLog(`checking if ${lat}, ${lon} is within ${radii.length} radii:`);
   verboseLog(radii);
   let success: { lat: number; lon: number } | undefined;
   const result = radii.some((radius) => {
-    const result = withinRadius({
-      lat,
-      lon,
-      radiusLat: radius.lat,
-      radiusLon: radius.lon,
-      radiusDiameterKm: radius.radius,
-    });
+    const result =
+      haversine(
+        { latitude: radius.lat, longitude: radius.lon },
+        { latitude: lat, longitude: lon },
+        { unit: "km" }
+      ) <= radius.diam;
     if (result) {
       success = radius;
     }
