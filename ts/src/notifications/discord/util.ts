685338d62a3c80da47919b0796535ce3e79a23af
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2024-01-24 10:53:56 -0500

    encapsulate conversion of Item -> discord embed

---
+++ ts/src/notifications/discord/util.ts
@@ -0,0 +1,48 @@
+import { Item, Platform } from "../../process.js";
+import Discord from "discord.js";
+
+const platformIcons: Record<Platform, string> = {
+  kijiji: "https://www.kijiji.ca/favicon.ico",
+  fb: "https://www.facebook.com/favicon.ico",
+};
+
+export const convertItemToDiscordEmbed = (item: Item) =>
+  new Discord.EmbedBuilder()
+    .setTitle(item.details.title ?? null)
+    .setDescription(
+      `- ${item.details.price ? `$${item.details.price}` : ""}\n- ${
+        item.details.location
+      }`
+    )
+    .setURL(item.url)
+    .setImage(item.imgUrl ?? null)
+    .setFooter({
+      text: item.platform,
+      iconURL: platformIcons[item.platform],
+    })
+    .setTimestamp(new Date());
+
+export const attachVideosToEmbed = (
+  embed: Discord.EmbedBuilder,
+  imgUrls: string[]
+) => {
+  const videos = imgUrls
+    .filter((src) => src.startsWith("http://img.youtube"))
+    .map((thumb) => {
+      const match = thumb.match(/(?:\/vi\/|v=)([^&\/\?]+)/)?.[1];
+      return match ? `https://youtu.be/${match}` : "[error]";
+    });
+  if (videos.length) {
+    embed.addFields(
+      videos.length > 1
+        ? {
+            name: `🎥 Video${videos.length > 1 ? "s" : ""}`,
+            value: videos.join("\n"),
+          }
+        : {
+            name: `🎥 Video: ${videos[0]}`,
+            value: " ",
+          }
+    );
+  }
+};
