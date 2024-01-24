4d958d3f4558f41b9a0c30450b1dfbc0012085e2
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2024-01-24 12:16:25 -0500

    introduce utils sanitizeString, trimAddress

--- ts/src/util/data.ts
+++ ts/src/util/data.ts
@@ -1,3 +1,4 @@
+import { Config } from "../types/config.js";
 export const findNestedProperty = (jsonString: string, key: string): any => {
   const keyIndex = jsonString.indexOf(`"${key}"`);
 
@@ -23,4 +24,25 @@ export const findNestedProperty = (jsonString: string, key: string): any => {
   }
 
   return undefined;
+};
+
+const sanitizeString = (s: string) =>
+  s
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase();
+
+export const trimAddress = (config: Config, address: string): string => {
+  const city = sanitizeString(config.search.location.city);
+  const prov = sanitizeString(config.search.location.region);
+  const cityIndex = sanitizeString(address).lastIndexOf(city);
+  const provIndex = sanitizeString(address).lastIndexOf(prov);
+  if (cityIndex === 0 || provIndex <= cityIndex) {
+    return address;
+  }
+  const result = address.substring(0, cityIndex).trim();
+  if (result[result.length - 1] === ",") {
+    return result.substring(0, result.length - 1);
+  }
+  return result;
 };
