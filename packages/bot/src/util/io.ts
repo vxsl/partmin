85f5a8216af50a60cb12e85aae7df3349478487b
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2024-02-12 13:27:30 -0500

    parseJSON doesn't throw if invalid JSON, instead returns undefined

--- packages/bot/src/util/io.ts
+++ packages/bot/src/util/io.ts
@@ -1,8 +1,17 @@
 import axios from "axios";
 import { createWriteStream, promises as fs } from "fs";
+import { log } from "util/log.js";
 
 // TODO use runtypes or something here:
-export const parseJSON = <T>(s: string): T => JSON.parse(s);
+export const parseJSON = <T>(s: string): T | undefined => {
+  try {
+    const parsed = JSON.parse(s);
+    return parsed;
+  } catch (e) {
+    log(`Error parsing JSON at path ${s}: ${e}`);
+  }
+  return undefined;
+};
 export const writeJSON = async (path: string, data: Object) =>
   await fs.writeFile(path, JSON.stringify(data, null, 2));
 
