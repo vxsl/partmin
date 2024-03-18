6ef7f04ef937392b04ec326ab3a1279ea0708221
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2024-03-18 14:26:24 -0400

    allow config.json to not exist on startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,12 +24,6 @@ case "$prog" in
     ;;
 esac
 
-if [ ! -f "./config/config.json" ]; then
-    echo "config/config.json not found, please create one from config/config.example.json and place it in the same directory"
-    exit 1
-fi
-
-
 function cleanup {
     echo "Sending SIGINT to child process: $child_pid"
     kill -s INT -$$
